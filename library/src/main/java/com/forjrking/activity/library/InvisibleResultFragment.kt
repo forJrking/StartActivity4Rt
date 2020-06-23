33e31ed88fe176eb405afc136bd9ae151573fd1b
qifj <qifj@novastar.tech>
2020-06-23 15:49:18 +0800

    //旋转屏幕问题修复

--- library/src/main/java/com/forjrking/activity/library/InvisibleResultFragment.kt
+++ library/src/main/java/com/forjrking/activity/library/InvisibleResultFragment.kt
@@ -1,8 +1,8 @@
 package com.forjrking.activity.library
 
 import android.content.Intent
+import android.os.Bundle
 import android.util.ArrayMap
-
 import androidx.fragment.app.Fragment
 import java.lang.ref.WeakReference
 
@@ -13,13 +13,22 @@ import java.lang.ref.WeakReference
  * @Version: 1.0.0
  */
 class InvisibleResultFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        //旋转屏幕不销毁
+        retainInstance = true
+        super.onCreate(savedInstanceState)
+    }
+
     /**防止内存泄漏*/
     private val callBacks: ArrayMap<Int, WeakReference<((Int, Int, Intent?) -> Unit)>> by lazy {
         ArrayMap<Int, WeakReference<((Int, Int, Intent?) -> Unit)>>()
     }
 
-    fun launchActivity(intent: Intent, requestCode: Int,
-        callBack: (requestCode: Int, resultCode: Int, data: Intent?) -> Unit) {
+    fun launchActivity(
+        intent: Intent, requestCode: Int,
+        callBack: (requestCode: Int, resultCode: Int, data: Intent?) -> Unit
+    ) {
         callBacks[requestCode] = WeakReference(callBack)
         startActivityForResult(intent, requestCode)
     }
